594db62602a3393d7dfad54ecdd334f8632e27c0
baihan <songtao_st08@163.com>
2023-04-01 14:42:38 +0800

    feat: init

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -28,7 +28,7 @@ export default async function (req, res) {
   try {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: generatePrompt(animal),
+      prompt: animal,
       temperature: 0.6,
     });
     res.status(200).json({ result: completion.data.choices[0].text });
